afa7b136580682572f7b5a83a7ae6c12f7c28952
mursaleen00 <playessport@gmail.com>
2025-12-10 03:03:26 +0500

    `Update`: Form handling

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -3,12 +3,19 @@ import { Input } from "@/components/ui/input";
 import { SignInSchema } from "@/schema/sign-in.schema";
 import { Fontisto } from "@expo/vector-icons";
 import { Link } from "expo-router";
-import { Formik } from "formik";
+import { useFormik } from "formik";
 import React from "react";
 import { Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Login() {
+  const { handleChange, isSubmitting, handleSubmit, errors, touched, values } =
+    useFormik({
+      validationSchema: { SignInSchema },
+      initialValues: { email: "", password: "" },
+      onSubmit: (values) => console.log("🚀 ~ Login ~ values:", values),
+    });
+
   return (
     <SafeAreaView>
       <View className="flex-col justify-between h-full">
@@ -20,62 +27,44 @@ export default function Login() {
 
           <Text className="text-text font-semibold text-3xl">Log In</Text>
 
-          <Formik
-            validationSchema={SignInSchema}
-            initialValues={{ email: "", password: "" }}
-            onSubmit={(values) => console.log("🚀 ~ Login ~ values:", values)}
-          >
-            {({
-              handleChange,
-              isSubmitting,
-              submitForm,
-              errors,
-              touched,
-              values,
-            }) => (
-              <View className="w-full flex-col gap-y-6">
-                <Input
-                  error={touched.email ? errors.email : undefined}
-                  onChangeText={handleChange("email")}
-                  placeholder="e.g.johndoe@gmail.com"
-                  label="Email Address"
-                  value={values.email}
-                  required
-                />
+          <View className="w-full flex-col gap-y-6">
+            <Input
+              error={touched.email ? errors.email : undefined}
+              onChangeText={handleChange("email")}
+              placeholder="e.g.johndoe@gmail.com"
+              label="Email Address"
+              value={values.email}
+              required
+            />
 
-                <Input
-                  error={touched.password ? errors.password : undefined}
-                  onChangeText={handleChange("password")}
-                  placeholder="Enter your password"
-                  value={values.password}
-                  secureTextEntry={true}
-                  label="Password"
-                  required
-                />
+            <Input
+              error={touched.password ? errors.password : undefined}
+              onChangeText={handleChange("password")}
+              placeholder="Enter your password"
+              value={values.password}
+              secureTextEntry={true}
+              label="Password"
+              required
+            />
 
-                <Link
-                  href={"/signup"}
-                  className="self-end border-b border-text"
-                >
-                  <Text className="font-semibold text-sm text-text">
-                    Forgot password?
-                  </Text>
-                </Link>
+            <Link href={"/signup"} className="self-end border-b border-text">
+              <Text className="font-semibold text-sm text-text">
+                Forgot password?
+              </Text>
+            </Link>
 
-                <Button
-                  text="Log in"
-                  onPress={submitForm}
-                  disabled={isSubmitting}
-                />
+            <Button
+              text="Log in"
+              onPress={handleSubmit}
+              disabled={isSubmitting}
+            />
 
-                <Button
-                  isOutline
-                  text="Sign up with Google"
-                  icon={<Fontisto size={24} name="google" color={"#0FA6A2"} />}
-                />
-              </View>
-            )}
-          </Formik>
+            <Button
+              isOutline
+              text="Sign up with Google"
+              icon={<Fontisto size={24} name="google" color={"#0FA6A2"} />}
+            />
+          </View>
         </View>
 
         <View className="flex-row w-fit self-center mb-6 gap-2">
